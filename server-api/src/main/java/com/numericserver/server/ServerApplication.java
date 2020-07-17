b9fa7d60b17c92661131bd273157daa326590f1f
MuradTcen <muradtcen@.gmail.com>
2020-07-17 17:13:01 +0500

    added back-api pom, properties, others

---
+++ server-api/src/main/java/com/numericserver/server/ServerApplication.java
@@ -0,0 +1,16 @@
+package com.numericserver.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@SpringBootApplication
+@Configuration
+public class ServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServerApplication.class, args);
+	}
+
+}
