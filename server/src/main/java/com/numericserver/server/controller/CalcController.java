43ae6246ff36d98a3f5f897b122a87408315db97
MuradTcen <muradtcen@.gmail.com>
2020-07-17 17:09:27 +0500

    added back CalcController

---
+++ server/src/main/java/com/numericserver/server/controller/CalcController.java
@@ -0,0 +1,22 @@
+package com.numericserver.server.controller;
+
+import com.numericserver.server.service.CalcService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/calc")
+@RequiredArgsConstructor
+@CrossOrigin(origins = {"http://localhost:3000", "localhost:3000"})
+public class CalcController {
+
+    private final CalcService calcServiceImpl;
+
+    @ResponseStatus(HttpStatus.OK)
+    @GetMapping
+    public int sum() {
+        return calcServiceImpl.calcNumbers().get();
+    }
+
+}
