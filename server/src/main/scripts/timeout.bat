ae3785ab63ad5a6425d94d7d77a6fd7ae9eaed49
MuradTcen <muradtcen@.gmail.com>
2020-07-21 11:58:06 +0500

    deleted comments from timeout.bat and pom.xml

--- server/src/main/scripts/timeout.bat
+++ server/src/main/scripts/timeout.bat
@@ -1,6 +1,4 @@
-set /A t= %~1+1
 echo "Sleeping for %~1 seconds…"
+set /A t= %~1+1
 ping 127.0.0.1 -n %t% > nul
-REM TIMEOUT /T %~1 /NOBREAK
-REM Здесь два варианта либо через хак "ping 127.0.0.1 -n %t% > nul" либо "REM TIMEOUT /T %~1 /NOBREAK"
 echo "Completed"
